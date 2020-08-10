3f19564ff89365ff805b84729b090eeea08a614d
xinyu <xinyu.olivier@epitech.eu>
2020-08-10 17:55:08 +0200

    add structure

---
+++ little-brewery-vue/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app" class="bg-gray-100 md:w-1/2 rounded px-6">
+   
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld
+  }
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
